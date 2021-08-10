0097074e03db7db1d527628ded42e6f82ec6d4d9
이철용 <ypopp1116@naver.com>
2021-08-10 13:54:31 +0900

    #4 Jenkins 파일 이름 중복 검사

--- BOM2Jenkinsfile.py
+++ BOM2Jenkinsfile.py
@@ -4,7 +4,6 @@ import clipboard
 
 # 파일 불러오기
 BOMfile = "BOM_20210804.xlsx"
-
 BOM = pd.read_excel(BOMfile, header=None)
 
 # 데이터 추출
@@ -30,13 +29,25 @@ try:
 except (ValueError, KeyError):
     pass
 
-# 파일 내용 복사, 붙여넣기
+# 파일 내용 복사
 f = open('JenkinsBuildSetting.txt', 'r')
 clipboard.copy(f.read())
 f.close()
 
+# Jenkins 파일 이름 중복 검사
 jenkinsname = "jenkinsfile"
+filename = jenkinsname
+number = 0
 
+while os.path.isfile(os.path.join("./", filename)):
+    overlap = str(input("같은 이름의 파일이 이미 있습니다. 덮어쓰시겠습니까?(y/n)\n"))
+    if overlap == "n":
+        number += 1
+        filename = jenkinsname + str(number)
+    elif overlap == "y":
+        break
+
+# 복사한 내용 붙여넣기
 f = open(jenkinsname, 'w')
 f.write("# Ver : %s\n# Data : %s\n# Author : %s\n\n" % (ver, data, Author))
 for i in range(len(list_ID)):
@@ -44,3 +55,5 @@ for i in range(len(list_ID)):
     i += 1
 f.write("\n\n" + clipboard.paste())
 f.close()
+
+print("Jenkinsfile is converted successfully!")
