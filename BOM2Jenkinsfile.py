e4ca71772e5a73f3367fd92d6b48ce94af9b7520
이철용 <ypopp1116@naver.com>
2021-08-10 13:09:50 +0900

    #2 필요한 데이터들을 추출하여 저장

--- BOM2Jenkinsfile.py
+++ BOM2Jenkinsfile.py
@@ -5,4 +5,26 @@ BOMfile = "BOM_20210804.xlsx"
 
 BOM = pd.read_excel(BOMfile, header=None)
 
-print(BOM)
+ver = BOM[1][0]
+data = BOM[1][1]
+Author = BOM[1][2]
+
+try:
+    column = 5
+    list_ID = []
+    while column >= 5 and BOM[1][column]:
+        list_ID.append(str(BOM[1][column]))
+        column += 1
+except (ValueError, KeyError):
+    pass
+print(list_ID)
+
+try:
+    column = 5
+    list_ver = []
+    while column >= 5 and BOM[2][column]:
+        list_ver.append(str(BOM[2][column]))
+        column += 1
+except (ValueError, KeyError):
+    pass
+print(list_ver)
